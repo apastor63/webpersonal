c499aa50189b19fceb5f52c64a4aec7d0ea5ce4e
apastor63 <apastor63@gmail.com>
2022-12-30 16:28:18 -0300

    Agregados al proyecto

---
+++ webpersonal/portfolio/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Titulo")
+    description = models.TextField(verbose_name="Descripción")
+    image = models.ImageField(verbose_name="Imagen", upload_to="projects")
+    link = models.URLField(verbose_name="Dirección web",null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True,verbose_name="Creado")
+    update = models.DateTimeField(auto_now=True,verbose_name="Actualizado")
+
+    class Meta:
+        verbose_name = "proyecto"
+        verbose_name_plural = "proyectos"
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.title
